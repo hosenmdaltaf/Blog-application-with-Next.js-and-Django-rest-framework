99996bc60a7761022e9dbbc3de7687413161233c
hosenmdaltaf <altafhmeazee1999@outlook>
2021-06-01 03:12:34 +0800

    make it better

--- blog/settings.py
+++ blog/settings.py
@@ -141,7 +141,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
